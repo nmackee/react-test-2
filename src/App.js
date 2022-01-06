6b8fb4bf95e0ff6130df624e31d41343e0a37987
nmackee <70114875+nmackee@users.noreply.github.com>
2022-01-07 02:10:19 +0900

    変数のメモ化(useMemo)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -12,6 +12,10 @@ export default function App() {
   const onClickOpen = () => setOpen(!open);
 
   const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  // 変数自体のメモ化
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
 
   return (
     <div className="App">
